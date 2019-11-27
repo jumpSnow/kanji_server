34a05d5dcad3d172f4d87d3485d9a27a22c9d481
jumpSnow <mwei6@ucsc.edu>
2019-11-27 09:59:15 -0800

    add server

---
+++ templates/precache-manifest.6457baa92ef9143eabb2b9fd106bc207.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ff4569893ddab4dd15d",
+    "url": "/../static/css/chunk-vendors.8d1713a3.css"
+  },
+  {
+    "revision": "d94e2b114fad355c9f45",
+    "url": "/../static/js/about.eadace00.js"
+  },
+  {
+    "revision": "e3be108f9c0804d80616",
+    "url": "/../static/js/app.dc0ca591.js"
+  },
+  {
+    "revision": "4ff4569893ddab4dd15d",
+    "url": "/../static/js/chunk-vendors.0b1d5a44.js"
+  },
+  {
+    "revision": "936b95488bdcdc3e068b1ddfe02abdbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95756a4b3b7979dc91a2fc09424e5d52",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ templates/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.6457baa92ef9143eabb2b9fd106bc207.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "kanji"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
